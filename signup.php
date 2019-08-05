17b2dbd20a737a3acf1a0a11737574e9c7c53a26
LihameA <53623816+LihameA@users.noreply.github.com>
2019-08-05 15:00:49 -0400

    Update signup.php

--- signup.php
+++ signup.php
@@ -20,10 +20,16 @@ return False;
 <form name="myForm" action= "Project1.html" onsubmit="return validate()" method="post">
   <div class="container">   <!-- creating the boxes using container witch is ab element in div -->
   <div class="main">
-  <h2>Javascript Login Form Validation</h2>
+  <center><h1>Unitool<h1><center>
+  <h2>Login Form Validation</h2>
   <form id="form_id" method="post" name="myform">
+
+  <label> First name:</label>
+  <h3><input type="text" name="First name" value=name..><h3>
+  <label>Last name: </label>
+  <h3><input type="text" name="Last name" value=name..><h3>
   <label>User Name :</label>
-  <input type="text" name="username" id="username"/>
+  <input type="text" user="username" id="username"/>
   <label>Password :</label>
   <input type="password" name="password" id="password"/>
   <input type="submit" value="submit"><link onclick=href="Project1.html"/>   <!-- accessing user input by calling the id back-->
